382d87edddfd08cc7da42403a87ba7e73a0b7f3b
M Sai Mahidhar <78961310+saimahidhar-18@users.noreply.github.com>
2023-11-01 11:55:48 +0530

    Update ImageCorusel.jsx

--- src/components/ImageCorusel.jsx
+++ src/components/ImageCorusel.jsx
@@ -8,28 +8,28 @@ const ImageCorusel = () => {
     <div className="mt-20 mb-10 flex flex-row">
         <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src={meet} className="w-full" />
+    <img src={meeting} className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src={b} className="w-full" />
+    <img src={meet} className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src={a} className="w-full" />
+    <img src={b} className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <img src={meeting} className="w-full" />
+    <img src={a} className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
