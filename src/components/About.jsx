08ebf9713585c01015196d4a1c869f0d5ae0bb97
M Sai Mahidhar <78961310+saimahidhar-18@users.noreply.github.com>
2023-11-01 11:42:38 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,7 +61,7 @@ const about = () => {
 
       <div className="text-center my-3">
         <button className="btn btn-active btn-info ">
-          <a href="tsudhakarreddy.in"> Register for this opportunity</a>
+          <a href="https://youtu.be/z7gw37NurOI"> Register for this opportunity</a>
         </button>
       </div>
     </div>
